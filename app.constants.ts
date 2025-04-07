0844d01dfab418353e66ac699a61eacc8dd6fdc4
MaximKashkevich <kashkevich.maxim@gmail.com>
2025-04-07 22:10:30 +0300

    add appwrite

---
+++ app.constants.ts
@@ -0,0 +1,6 @@
+export const APP_WRITE_ID = '67f40dbf002904329a3a'
+export const DB_ID = 'deals'
+export const COLLECTION_DEALS = 'crm-deals'
+export const COLLECTION_CUSTOMERS = 'customers'
+export const COLLECTION_COMMENTS = 'comments'
+export const STORAGE_ID = 'storage'
